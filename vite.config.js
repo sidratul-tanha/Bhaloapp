483f941de6156db582563c3f0336f51c7153c568
Your Name <you@example.com>
2025-11-04 10:35:41 +0600

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-    base: '/BhaloAchoTo/',
-  plugins: [react()],
+    base: '/', // <--- root for Vercel
+    plugins: [react()],
 })
 
 
